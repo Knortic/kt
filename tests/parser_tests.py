4d6cd03521bf79fb503ce4c57b4ffc684891d236
Knortic <freefloweto@gmail.com>
2025-05-14 03:39:13 +0100

    Red - Checking if first command line arg is a number.

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -1,6 +1,14 @@
 from src.parser import CommandLineArgsParser
 
+parser = CommandLineArgsParser()
+
 def test_is_command_line_args_greater_than_zero():
-    parser = CommandLineArgsParser()
     cmd_line_args = parser.fetch_args()
     assert len(cmd_line_args) > 0
+
+def test_is_first_command_line_arg_a_number():
+    cmd_line_args = parser.fetch_args()
+    if cmd_line_args[0].isnumeric():
+        assert True
+    else:
+        assert False
