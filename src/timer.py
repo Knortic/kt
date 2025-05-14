c082a3335d6668d40b1189e8ca8c8dd8eb4dcf4e
Knortic <freefloweto@gmail.com>
2025-05-14 20:36:54 +0100

    Green - After timer is started, should be active.

--- src/timer.py
+++ src/timer.py
@@ -3,9 +3,14 @@ from src.parser import CommandLineArgsParser
 class Timer:
     def __init__(self, parser):
         self.parser = parser
+        self.active = False
 
     def is_active(self):
-        return False
+        return self.active
 
     def create(self):
         return True
+
+    def start(self):
+        self.active = True
+
