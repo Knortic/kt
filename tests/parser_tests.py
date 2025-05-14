71ded26709049a7d1be60969e7cb983c8f4446af
Knortic <freefloweto@gmail.com>
2025-05-14 04:17:40 +0100

    Green - Checking if second command line arg is a string.

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -1,7 +1,7 @@
 import pytest
 from src.parser import CommandLineArgsParser, InvalidArgumentError
 
-parser = CommandLineArgsParser("0")
+parser = CommandLineArgsParser("0", "Focus on work for 30 minutes")
 
 def test_is_command_line_args_greater_than_zero():
     args = parser.process_args()
@@ -16,4 +16,8 @@ def test_given_command_line_arg_amount_above_two_should_throw_exception():
 
     with pytest.raises(InvalidArgumentError):
         invalid_parser.process_args()
+
+def test_is_second_command_line_arg_a_string():
+    args = parser.process_args()
+    assert isinstance(args[1], str)
 
