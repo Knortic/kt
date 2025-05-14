a981cb105377a501d760dce43f6989ec35a2990e
Knortic <freefloweto@gmail.com>
2025-05-14 20:13:47 +0100

    Green - Checking if a five minute timer can be created.

---
+++ src/timer.py
@@ -0,0 +1,8 @@
+from src.parser import CommandLineArgsParser
+
+class Timer:
+    def __init__(self, parser):
+        self.parser = parser
+
+    def create(self):
+        return True
--- tests/timer_tests.py
+++ tests/timer_tests.py
@@ -1,4 +1,5 @@
 import pytest
+from src.timer import Timer
 from src.parser import CommandLineArgsParser
 
 def test_can_create_five_minute_timer():
