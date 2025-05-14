ca201056b4b8f96dd21fb65d2a86767946b7364f
Knortic <freefloweto@gmail.com>
2025-05-14 20:10:51 +0100

    Red - Checking if a five minute timer can be created.

---
+++ tests/timer_tests.py
@@ -0,0 +1,8 @@
+import pytest
+from src.parser import CommandLineArgsParser
+
+def test_can_create_five_minute_timer():
+    parser = CommandLineArgsParser("5m", "This should finish in 5 minutes")
+    t = Timer(parser)
+    has_created_timer = t.create()
+    assert has_created_timer
