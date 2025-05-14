a69099f8bac87bcb553945556333c483f5822909
Knortic <freefloweto@gmail.com>
2025-05-14 04:56:12 +0100

    Refactor - Checking against first command line arg being negative.

--- src/parser.py
+++ src/parser.py
@@ -6,8 +6,11 @@ class CommandLineArgsParser:
         self.args = list(args)
 
     def process_args(self):
-        if len(self.args) > 2:
+        is_time_negative = int(self.args[0]) < 0
+        has_too_many_args = len(self.args) > 2
+
+        if has_too_many_args:
             raise InvalidArgumentError("Arguments exceeded size of 2!")
-        elif (int(self.args[0]) < 0):
+        elif (is_time_negative):
             raise InvalidArgumentError("Specified time cannot be negative!")
         return self.args
