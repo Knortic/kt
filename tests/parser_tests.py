249a55ef814654532a1c6af9147a4dac623a8c36
Knortic <freefloweto@gmail.com>
2025-05-14 04:00:59 +0100

    Red - Checking against command line args being greater than 2.

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -1,3 +1,4 @@
+import pytest
 from src.parser import CommandLineArgsParser
 
 parser = CommandLineArgsParser("0")
@@ -9,4 +10,10 @@ def test_is_command_line_args_greater_than_zero():
 def test_is_first_command_line_arg_a_number():
     args = parser.process_args()
     assert args[0].isnumeric()
+
+def test_given_command_line_arg_amount_above_two_should_throw_exception():
+    invalid_parser = CommandLineArgsParser("0", "test", "invalid")
+
+    with pytest.raises(InvalidArgumentError):
+        parser.process_args()
 
