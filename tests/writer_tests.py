b0718f3436829f98e375dfa398820301a0da5cca
Knortic <freefloweto@gmail.com>
2025-07-07 05:35:17 +0100

    Red - Fake file writer calling write, should have valid returned filename.

--- tests/writer_tests.py
+++ tests/writer_tests.py
@@ -6,3 +6,8 @@ def test_can_call_write():
     w = FakeFileWriter(datetime.now())
     w.write()
     assert w.write_amount > 0
+
+def test_write_is_called_has_valid_returned_filename():
+    w = FakeFileWriter(datetime.now())
+    w.write()
+    assert w.returned_filename != ""
