78d34d79f78beb0bb73182c3326c0546fba78205
Knortic <freefloweto@gmail.com>
2025-07-07 06:08:08 +0100

    Green - When timer is started, should contain non empty filename.

--- tests/timer_tests.py
+++ tests/timer_tests.py
@@ -42,11 +42,18 @@ def test_when_timer_is_started_should_return_write_amount_greater_than_zero():
     t.start()
     assert fake_writer.write_amount > 0
 
-def test_when_timer_is_started_twice_without_stopping_should_return_write_amount_equals_one():
+def test_when_timer_is_started_twice_without_stopping_should_contain_write_amount_equals_one():
     fake_writer = create_fake_file_writer()
     t = Timer(parser, fake_writer)
     validate_timer(t)
     t.start()
     t.start()
     assert fake_writer.write_amount == 1
+
+def test_when_timer_is_started_should_contain_non_empty_filename():
+    fake_writer = create_fake_file_writer()
+    t = Timer(parser, fake_writer)
+    validate_timer(t)
+    t.start()
+    assert fake_writer.returned_filename != ""
 
