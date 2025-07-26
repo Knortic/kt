e524b675c268ebc0442441ea8551deaa3cbcf222
Knortic <freefloweto@gmail.com>
2025-07-26 16:07:27 +0100

    Clean slate.

--- src/errors.py
+++
@@ -1,3 +0,0 @@
-class InvalidArgumentError(Exception):
-    pass
-
--- src/timer_data.py
+++
@@ -1,6 +0,0 @@
-class TimerData:
-    def __init__(self, timestamp, message, duration):
-        self.timestamp = timestamp
-        self.message = message
-        self.duration = duration
-
