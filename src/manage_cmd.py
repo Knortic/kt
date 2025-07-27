823eb689b0137509ab0a7bd1993be778ba186214
Knortic <freefloweto@gmail.com>
2025-07-27 05:56:22 +0100

    Added stop/reset functionality along with refactoring pause logic for manage command.

--- src/manage_cmd.py
+++ src/manage_cmd.py
@@ -1,8 +1,28 @@
 import json
 
+cmds = [ "pause", "unpause", "toggle-pause",
+         "stop", "reset"
+       ]
+
+def handle_pause_state(json_obj, timer_id, args):
+    pause_state = json_obj[timer_id]["paused"] 
+
+    if args[2] == "pause":
+        pause_state = True
+    elif args[2] == "unpause":
+        pause_state = False
+    elif args[2] == "toggle-pause":
+        pause_state = not pause_state
+
+    json_obj[timer_id]["paused"] = pause_state
+
 def handle_manage_cmd(timers_filepath, args):
     if len(args) <= 1:
         # TODO: Actually print to shell properly probs using stdout ("Invalid argument count")
+        return
+
+    if not args[2] in cmds:
+        # TODO: Actually print to shell properly probs using stdout ("Unrecognized command")
         return
 
     timer_id = int(args[1])
@@ -18,19 +38,10 @@ def handle_manage_cmd(timers_filepath, args):
                     # TODO: Actually print to shell properly probs using stdout ("Invalid id provided")
                     return
 
-                pause_state = None
+                handle_pause_state(json_obj, timer_id, args)
 
-                if args[2] == "pause":
-                    pause_state = True
-                elif args[2] == "unpause":
-                    pause_state = False
-                elif args[2] == "toggle-pause":
-                    pause_state = not json_obj[timer_id]["paused"]
-                else:
-                    # TODO: Actually print to shell properly probs using stdout ("Invalid command provided for timer")
-                    return
-
-                json_obj[timer_id]["paused"] = pause_state
+                if args[2] == "stop" or args[2] == "reset":
+                    json_obj[timer_id]["reset"] = True
 
                 with open(timers_filepath, "w") as file_handle:
                     json.dump(json_obj, file_handle, indent=2)
